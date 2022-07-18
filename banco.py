b2eba599c101a24f0bbc51fdb6e251a1b920c595
Andres Suarez <santiagosuarez.99@hotmail.com>
2022-07-18 16:31:51 -0500

    Clase persona terminada

--- banco.py
+++ banco.py
@@ -1,5 +1,21 @@
-class Perosna:
-    pass
+class Persona:
+    def __init__(self, nombre: str, apellido: str, documento: int, saldo: float) -> None:
+        self.nombre = nombre
+        self.apellido = apellido
+        self.documento = documento
+        self.__saldo = saldo
+    
+    def __str__(self) -> str:
+        return f"Nombre: {self.nombre} || Apellido: {self.apellido} || Documento: {self.documento}"
+    
+    def get_saldo(self) -> float:
+        return self.__saldo
+    
+    def realizar_pago(self, monto: float):
+        self.__saldo -= monto
+    
+    def realizar_deposito(self, monto: float):
+        self.__saldo += monto
 
 class Banco:
     def __init__(self, nombre: str, direccion: str) -> None:
